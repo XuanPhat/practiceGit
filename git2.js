7f53bcfe63d8c17eca5d97f36d6598e118436d32
XuanPhat <phatnx2000@gmail.com>
2023-05-25 22:43:31 +0700

    phan 1

---
+++ git2.js
@@ -0,0 +1,40 @@
+function meo(meo) {
+  if (meo == true) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function phan2(meo) {
+  if (meo == true) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function phan3(meo) {
+  if (meo == true) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function phan4(meo) {
+  if (meo == true) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function phan5(meo) {
+  if (meo == true) {
+    return true
+  } else {
+    return false
+  }
+}
+
